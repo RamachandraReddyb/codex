9d7847d7c2d73b5009c052b8c7b3ab5f5dd07b9a
B Ramachandra Reddy <ramachandra.reddy@impelsys.com>
2023-01-05 13:28:17 +0530

    updated server url

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from openai
 
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://botcode-ynvm.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
